22e1d8b90dec119809e027fdc3563b8873d875c8
knutfx <knutfx@gmail.com>
2024-12-13 20:13:33 +0100

    Fix repository structure for HACS

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+const resolve = require('@rollup/plugin-node-resolve');
+const terser = require('rollup-plugin-terser').terser;
 
-export default {
+module.exports = {
   input: 'src/heating-curve-card.js',
   output: {
-    dir: 'dist',
+    file: 'heating-curve-card.js',
     format: 'es',
   },
   plugins: [
     resolve(),
-    terser(),
-  ],
+    terser()
+  ]
 };
